1bcef1aedbcd45abf0dced338a3433b2c7361e14
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2017-01-16 19:58:29 +0900

    suwarun更新

--- suwarun/main.js
+++ suwarun/main.js
@@ -1,5 +1,5 @@
 var example2 = new Vue({
-  el: '#chair',
+  el: '#reserved_chair_1',
   data: {
 		imageUrl: './images/free-chair.png',
     free_url: './images/free-chair.png',
@@ -8,7 +8,47 @@ var example2 = new Vue({
   // `methods` オブジェクトの下にメソッドを定義する
   methods: {
     chair_change_status: function (event) {
-      this.imageUrl = this.reserved_url
+      if ( this.imageUrl == this.reserved_url ){
+				this.imageUrl = this.free_url
+			}else if (this.imageUrl == this.free_url) {
+				this.imageUrl = this.reserved_url
+			}
     }
   }
+})
+
+
+
+
+var chair = Vue.extend({
+	data: function () {
+		return {
+			imageUrl: './images/free-chair.png'
+		}
+	},
+		methods: {
+	    change_status: function (event) {
+	      if ( this.imageUrl == this.reserved_url ){
+					this.imageUrl = this.free_url
+				}else if (this.imageUrl == this.free_url) {
+					this.imageUrl = this.reserved_url
+				}
+	    }
+	  },
+		computed: {
+			URL : function(){
+					return this.imageUrl
+			},
+		},
+		template: '<img src="./images/free-chair.png" class="leftward chair">{{URL}}</img>'
+});
+
+var conferenceRoom = new Vue({
+		el: '#conferenceRoom',
+		data: {
+			imageUrl: './images/free-chair.png'
+		},
+		components: {
+				'chair': chair
+		}
 })
