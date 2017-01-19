8154f0b2467b931d2383880f26e1d4955886ac1b
Takuya Endo <endo@Takuyas-MacBook-Pro.local>
2017-01-19 18:36:18 +0900

    no message

--- suwarun/main.js
+++ suwarun/main.js
@@ -21,34 +21,66 @@ var example2 = new Vue({
 
 
 var chair = Vue.extend({
-	data: function () {
-		return {
-			imageUrl: './images/free-chair.png'
-		}
-	},
-		methods: {
-	    change_status: function (event) {
-	      if ( this.imageUrl == this.reserved_url ){
-					this.imageUrl = this.free_url
-				}else if (this.imageUrl == this.free_url) {
-					this.imageUrl = this.reserved_url
-				}
-	    }
-	  },
+
+	  props: ['reserved'],
+
 		computed: {
 			URL : function(){
-					return this.imageUrl
-			},
+
+					if (this.reserved) {
+						return './images/reserved-chair.png'
+					}else {
+						return './images/free-chair.png'
+					}
+
+			}
 		},
-		template: '<img src="./images/free-chair.png" class="leftward chair">{{URL}}</img>'
+		template: '<img v-bind:src="URL" class="leftward chair">'
 });
 
 var conferenceRoom = new Vue({
 		el: '#conferenceRoom',
-		data: {
-			imageUrl: './images/free-chair.png'
-		},
 		components: {
 				'chair': chair
+		},
+		data: function(){
+			reserved : false
+
+		},
+		methods: {
+			change_status: function (event) {
+				if ( this.reserved ){
+					this.reserved = false
+				}else if (!this.reserved) {
+					this.reserved = true
+				}
+				console.log(this.reserved)
+			}
+		},
+
+})
+
+
+var freeSpace = new Vue({
+		el: '#freeSpace',
+		components: {
+				'table': table
 		}
 })
+
+var table = Vue.extend({
+
+	  props: ['reserved'],
+		computed: {
+			chairURL : function(){
+
+					if (reserved == true) {
+						return './images/reserved-chair.png'
+					}else {
+						return './images/free-chair.png'
+					}
+
+			}
+		},
+		template: '<img v-bind:src="chairURL" class="rightward chair"><div class="circle"></div><img v-bind:src="chairURL" class="leftward chair">'
+});
